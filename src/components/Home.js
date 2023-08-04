8e2b9d8238d300ed5e5a1860fc66db5dce6f3a38
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2023-08-04 19:00:46 +0200

    Search city service

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,44 +3,65 @@ import React, { Component } from "react";
 import "./css/Home.css";
 import SearchField from "./SearchField";
 import CityInformation from "./CityInformation";
-
-import 'bootstrap/dist/css/bootstrap.css';
 import Login from "./Login";
 import WeatherForecast from "./WeatherForecast";
 import PopulationInfo from "./PopulationInfo";
 import ExchangeRatesInfo from "./ExchangeRatesInfo";
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 class Home extends Component {
   state = {
     searchText: "",
     searchData: [],
-    cityName: "Maputo",
-    countryName: "Mozambique",
-    currentTemperature: "20",
-    minTemperature: "15",
-    maxTemperature: "21",
-    humidity: "3",
-    currentPopulation: "33.524.125",
-    gdp: "521.452325",
-    base: "MZN",
+    cityName: "",
+    countryName: "",
+    currentTemperature: "",
+    minTemperature: "",
+    maxTemperature: "",
+    humidity: "",
+    currentPopulation: "",
+    gdp: "",
+    base: "",
   };
 
   callback = (data) => {
-    this.setState({searchData: data})
-    console.log(this.state.searchData)
-    }
+    this.setState({ searchData: data });
+    this.setState({
+      cityName: this.state.searchData.city.name });
+    this.setState({ countryName: this.state.searchData.gdpPerCapita.country.value });
+    this.setState({ currentTemperature: this.state.searchData.city.weather.currentTemperature });
+    this.setState({ minTemperature: this.state.searchData.city.weather.minTemperature });
+    this.setState({ maxTemperature: this.state.searchData.city.weather.maxTemperature });
+    this.setState({ humidity: this.state.searchData.city.weather.humidity });
+    this.setState({ currentPopulation:  "33.524.125" });
+    this.setState({ gdp: this.state.searchData.gdpPerCapita.value });
+    this.setState({ base: this.state.searchData.exchangeRate.rates[0].id });
+    console.log(this.state.searchData.city.name)
+  }
 
   render() {
     return (
       <>
-        <div className="div-home">
-          <SearchField onClick={this.callback} />
-          <CityInformation cityName={this.state.searchData} countryName={this.state.countryName} />
-          <WeatherForecast currentTemperature={this.state.currentTemperature} minTemperature={this.state.minTemperature}
-            maxTemperature={this.state.maxTemperature} humidity={this.state.humidity} />
-          <PopulationInfo currentPopulation={this.state.currentPopulation} gdp={this.state.gdp} />
-          <ExchangeRatesInfo base={this.state.base} />
+        <div className="div-home-main">
+          <div className="container">
+            <div class="row">
+              <div class="col"><SearchField onClick={this.callback} /></div>
+            </div>
+            <div class="row">
+              <div class="col"><CityInformation cityName={this.state.cityName} countryName={this.state.countryName} /></div>
+            </div>
+            <div class="row">
+              <div class="col"><WeatherForecast currentTemperature={this.state.currentTemperature} minTemperature={this.state.minTemperature}
+                maxTemperature={this.state.maxTemperature} humidity={this.state.humidity} /></div>
+            </div>
+            <div class="row">
+              <div class="col"><PopulationInfo currentPopulation={this.state.currentPopulation} gdp={this.state.gdp} /></div>
+              <br /><br /><br /><br />
+              <div class="col"><ExchangeRatesInfo base={this.state.base} /></div>
+            </div>
+          </div>
         </div>
       </>
     );
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -10,38 +10,43 @@ class SearchField extends Component {
   };
 
   onSearchInputChange = (event) => {
+    event.preventDefault();
     this.setState({ searchText: event.target.value });
   }
 
   searchCityInfo = () => {
     axios({
       method: "GET",
-      url: "http://localhost:8080/v1/search/city/"+this.state.searchText
+      url: "http://localhost:8080/v1/search/city/" + this.state.searchText
     }).then((response) => {
       const data = response.data
-      this.setState({searchData: data})
+      this.setState({ searchData: data })
       this.props.onClick(this.state.searchData)
-      console.log(this.state.searchData)
     }).catch((error) => {
-      if(error.response) {
+      if (error.response) {
         console.log(error.response);
         console.log(error.response.status);
         console.log(error.response.headers);
-        this.props.onClick(error.response.status)
       }
     })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
   }
 
   render() {
     return (
       <>
-        <div className="d-flex align-items-center justify-content-center vh-100 flex-column gap-4">
-          <form className="d-flex">
-            <input className="form-control me-1 search-field" type="search"
-              placeholder="Type a destination city name here..." aria-label="Search"
-              value={this.state.searchText} onChange={this.onSearchInputChange} />
-            <button className="btn btn-outline-primary" type="submit" onClick={this.searchCityInfo}>Search</button>
-          </form>
+        <div className="div-home-main">
+          <div className="form-group has-search">
+            <form className="d-flex" onSubmit={this.handleSubmit}>
+              <input className="form-control me-1 search-field" type="search"
+                placeholder="Type a destination city name here..." aria-label="Search"
+                value={this.state.searchText} onChange={this.onSearchInputChange} onKeyPress={this.searchCityInfo} />
+              <button className="btn btn-outline-primary" type="submit" onClick={this.searchCityInfo}>Search</button>
+            </form>
+          </div>
         </div>
       </>
     );
